8e5351d2622446070522645103bceceb89bdb8c3
Pallavi Deore <pallavi.deore@VEROLT.COM>
2022-09-29 13:54:11 +0530

    code change

--- generic.cpp
+++ generic.cpp
@@ -3,11 +3,16 @@ using namespace std;
 
 int main() {
 
-  int first_number, second_number, sum;
+  int first_number, second_number, sum ,product;
     
   cout << "Enter two integers: ";
   cin >> first_number >> second_number ;
 
   sum = first_number + second_number ;
 
+  product = first_number * second_number ;
+
+
   cout << first_number << " + " <<  second_number << " + " <<  " = " << sum; 
+
+  cout << first_number << " + " <<  second_number << " * " <<  " = " << product;
